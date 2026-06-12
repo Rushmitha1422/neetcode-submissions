85dfc1a6317a48d2ec9c35172a07812e7b818514
Ubbara Rushmitha <155905979+Rushmitha1422@users.noreply.github.com>
2026-06-12 22:42:43 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+                count++;
+            }else if(s.charAt(i) != t.charAt(j)){
+                j++;
+            }else if(j==t.length())
+                i++;
+        }
+        return count == s.length();
+    }
+}
